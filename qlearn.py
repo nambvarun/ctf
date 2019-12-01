4187ae30621313d55e2bf55a0e8e88fc38236426
Thurston <tbrevett@stanford.edu>
2019-12-01 13:29:10 -0800

    Updated grid definitions

--- qlearn.py
+++ qlearn.py
@@ -1,24 +1,34 @@
 import numpy as np
 import scipy as sp
+import itertools
 
 # The state action grid: all matrices are defined on this grid
 class SAGrid:
     
     # resolution and extent of the state-action space:
-    # start, resolution, stop(exclusive)
-    xdef = [0, 1, 10]
-    ydef = [0, 1, 10]
-    rdef = [0, 0.25, 1]
-    thdef = [0, 30, 360]
+    # start, stop(exclusive), step
+    xdef = [0, 10, 1] # state - x: 
+    ydef = [0, 10, 1] # state - y
+    rdef = [0, 1, 0.25] # action - range
+    thdef = [0, 360, 30] # action - angle (deg)
 
-    def defSize(self, obdef):
-        return ((obdef[2] - obdef[0]) / obdef[1]) - 1
-
-    def getSSize(self):
-        return (self.defSize(self.xdef), self.defSize(self.ydef))
     
-    def getSASize(self):
-        return (self.defSize(self.xdef), self.defSize(self.ydef), self.defSize(self.rdef), self.defSize(self.thdef))
+    def getNumActions(self):
+        return len(list(np.arange(*self.rdef))) * len(list(np.arange(*self.thdef)]))        
+
+    def getNumStates(self):
+        return len(list(np.arange(*self.xdef))) * len(list(np.arange(*self.ydef)]))
+    
+    def getNumStateActions(self):
+        return self.getNumActions() * self.getNumStates()
+    
+    def getStates(self):
+        return itertools.product(np.arange(*self.xdef), np.arange(*self.ydef))
+
+    def getActions(self):
+        return itertools.product(np.arange(*self.rdef), np.arange(*self.thdef))
+    
+
 
 
 # define the action-utility functon for the Qmat (i.e. value of Q @ s, a)
@@ -37,9 +47,9 @@ def main():
     sagrid = SAGrid()
 
     # define matrices for Q-learning and reward estimation
-    Qmat = np.zeros(sagrid.getSASize(), dtype=np.single) # sampled state action Q 
-    Rmat = sp.sparse.coo_matrix(sagrid.getSSize(), dtype=np.single) # sampled reward function
-    Pf   = np.ones(sagrid.getSASize(), dtype=np.bool) # scaled belief on the flag position
+    Qmat = np.zeros(sagrid.getNumStateActions(), dtype=np.single) # sampled state action Q 
+    Rmat = sp.sparse.coo_matrix(sagrid.getNumStates(), dtype=np.single) # sampled reward function
+    Pf   = np.ones(sagrid.getNumStates(), dtype=np.bool) # scaled belief on the flag position
     Map = [] # lines in the map
 
     # define function for the reward of a state (action)
