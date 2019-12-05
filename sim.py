393c46a26cedb4698902136e582f983b2ce0ebd0
Varun Nambiar <nambvarun@gmail.com>
2019-12-04 20:07:00 -0800

    wip: classes for simulation

---
+++ sim.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class Simulation:
+    def __init__(self):
+        print('hello')
+
+    def render(self):
+        return("bleh")
+
+
+# class Obstacle:
+#
+#
+# class Agent:
